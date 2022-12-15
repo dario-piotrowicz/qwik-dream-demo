1d43bee90ca0c3af2d649fa9a94b114459c4c9a6
dario-piotrowicz <dario.piotrowicz@gmail.com>
2022-12-15 00:20:36 +0000

    add close not found reproduction

--- apps/host/src/routes/index.tsx
+++ apps/host/src/routes/index.tsx
@@ -1,18 +1,26 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useSignal } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 import ComponentLoader from '~/components/component-loader/ComponentLoader';
 import RemoteSsr from '~/components/remote-ssr';
 import { remotes } from '../../../../libs/shared/remotes';
 
 export default component$(() => {
+	const myBool = useSignal<boolean>(false);
+
 	return (
-		<main>
-			<RemoteSsr remote={remotes.menu} />
-			<RemoteSsr remote={remotes.cart} />
-			<RemoteSsr remote={remotes.hero} />
-			<RemoteSsr remote={remotes.product} />
-			<ComponentLoader remote={remotes.reviews} />
-		</main>
+		<>
+			<button style="background-color: green;" onClick$={() => (myBool.value = !myBool.value)}>click me</button>
+			<main>
+				<RemoteSsr remote={remotes.menu} />
+				<RemoteSsr remote={remotes.cart} />
+				<RemoteSsr remote={remotes.hero} />
+				<RemoteSsr remote={remotes.product} />
+				<ComponentLoader remote={remotes.reviews} />
+			</main>
+			<div style="position: fixed; top: 0; right: 0; background-color: red;">
+				<p>{`>>> ${myBool.value} <<<`}</p>
+			</div>
+		</>
 	);
 });
 
